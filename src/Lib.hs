9d418bb2696c3bb77233f931c00a04f6c97ae208
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2016-06-21 17:09:35 +0500

    add reading config from config.local

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,8 +12,9 @@ import Data.ByteString.Lazy as BSL hiding (putStrLn)
 import Codec.Binary.UTF8.String as UTF8 (decode)
 import Network.Wreq
 import Control.Lens
+import Data.Configurator
 
-import Prelude hiding (putStrLn)
+import Prelude hiding (putStrLn, lookup)
 import Data.String.Class (putStrLn)
 import Data.Aeson.Lens
 import Data.Aeson
@@ -21,9 +22,6 @@ import Data.Aeson
 import Data.Time.Clock (UTCTime)
 
 import GHC.Generics
-
-getBase64BearerCredentials :: C.ByteString
-getBase64BearerCredentials = B64.encode $ C.pack (consumerKey ++ ":" ++ consumerSecret)
 
 data Timeline = Timeline { statuses :: [Tweet]
                          } deriving (Generic)
@@ -49,18 +47,37 @@ getTweetsFromResponse :: Response BSL.ByteString -> Either String Timeline
 getTweetsFromResponse r = eitherDecode $ r ^. responseBody
 
 
+data Credentials = Credentials { consumerKey :: String, consumerSecret :: String }
+
+getCredentials :: IO (Maybe Credentials)
+getCredentials = do
+  (config, _) <- autoReload autoConfig [("config.local")]
+  res <- lookup config "consumerKey"
+  res2 <- lookup config "consumerSecret"
+  case res of
+    Nothing -> return $ Nothing
+    Just a  -> case res2 of
+      Nothing -> return $ Nothing
+      Just b -> return $ Just  $ Credentials { consumerKey = a, consumerSecret = b }
+
+getBase64BearerCredentials :: Credentials -> C.ByteString
+getBase64BearerCredentials Credentials {consumerKey = a, consumerSecret = b} = B64.encode $ C.pack (a ++ ":" ++ b)
+
 someFunc :: IO ()
-someFunc =
-  do
-  let opts = defaults & header "Authorization" .~ [C.pack $ "Basic " ++ C.unpack  getBase64BearerCredentials] & header "Content-Type" .~ ["application/x-www-form-urlencoded;charset=UTF-8"]
-  r <- postWith opts "https://api.twitter.com/oauth2/token" (C.pack "grant_type=client_credentials")
-  case access_token r of
-    Nothing -> putStrLn ("Nothing 1" :: String)
-    Just token -> do
-      let n_opts = defaults & header "Authorization" .~ [C.pack $ "Bearer " ++ T.unpack token]
-      d <- getWith n_opts ("https://api.twitter.com/1.1/search/tweets.json?q=" ++ "haskell")
-      case getTweetsFromResponse d of
-        Left str -> putStrLn str
-        Right xs -> putStrLn $ show xs
-  where
-    access_token _r = _r ^? responseBody . key "access_token" . _String
+someFunc = do
+  credentials <- getCredentials
+  case credentials of
+    Nothing -> putStrLn ("config.local doesn't exist or has invalid format" :: String)
+    Just cr ->  do
+      let opts = defaults & header "Authorization" .~ [C.pack $ "Basic " ++ C.unpack (getBase64BearerCredentials cr)] & header "Content-Type" .~ ["application/x-www-form-urlencoded;charset=UTF-8"]
+      r <- postWith opts "https://api.twitter.com/oauth2/token" (C.pack "grant_type=client_credentials")
+      case access_token r of
+        Nothing -> putStrLn ("Nothing 1" :: String)
+        Just token -> do
+          let n_opts = defaults & header "Authorization" .~ [C.pack $ "Bearer " ++ T.unpack token]
+          d <- getWith n_opts ("https://api.twitter.com/1.1/search/tweets.json?q=" ++ "golang")
+          case getTweetsFromResponse d of
+            Left str -> putStrLn str
+            Right xs -> putStrLn $ show xs
+      where
+        access_token _r = _r ^? responseBody . key "access_token" . _String
